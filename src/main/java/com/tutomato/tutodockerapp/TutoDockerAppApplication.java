c1ee878fbf9b2684ead9e6c93d2509963051008e
jongwan <jongbell4@gmail.com>
2024-09-13 15:16:09 +0900

    docker tutorial web app repository

---
+++ src/main/java/com/tutomato/tutodockerapp/TutoDockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.tutomato.tutodockerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoDockerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutoDockerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tutomato/tutodockerapp/TutoDockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutomato.tutodockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoDockerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
